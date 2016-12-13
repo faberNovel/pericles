60a6a16e304cd7018463370e88655313ff9b016d
Charles-Elie Simon <charles-elie.simon@applidium.com>
2016-12-13 16:48:33 +0100

    FIX #37860 Adding new, create and show actions in projects controller (with tests and views). Modifying navbar (link to create projects)
    Change-Id: I3cd01ce5e404409e83148ad72db78981e8cd6503

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,37 @@
 class ProjectsController < ApplicationController
 
-	def index
-		@projects = Project.all
-	end
+  before_action :setup_project, only: [:show]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def setup_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 
 end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
+
+	setup do
+		Project.delete_all
+		@project = create(:project)
+	end
+
 	test "should get index" do
 		get projects_path
 		assert_response :success
 	end
+
+	test "should show project" do
+		get project_url(@project)
+		assert_response :success
+	end
+
+	test "should get new" do
+		get new_project_path
+		assert_response :success
+	end
+
+	test "should create project" do
+		assert_difference('Project.count') do
+      post projects_url, params: { project: { description: 'My description', title: 'My Project' }}
+		end
+    assert_redirected_to project_url(Project.last)
+	end
+
+	test "should not create project" do
+		assert_no_difference('Project.count') do
+      post projects_url, params: { project: { description: 'My description', title: nil }}
+		end
+    assert_response :success
+	end
+
 end
