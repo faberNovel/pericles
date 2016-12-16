52bf8e7c02202b5fa61e886e6994cc8cd9e89378
Charles-Elie Simon <charles-elie.simon@applidium.com>
2016-12-16 15:52:40 +0100

    WIP #37858 Adding json_schemas as resources in routes.rb
    Change-Id: I469a0f19c5cc317445378c6b7e287249ea11a364

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-	resources :projects
-	root "projects#index"
+  resources :projects do
+    resources :json_schemas
+  end
+  root "projects#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
