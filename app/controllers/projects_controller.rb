f102d14721acc99a11153c4689ea12bfad2e16e8
Charles-Elie Simon <charles-elie.simon@applidium.com>
2016-12-16 15:59:23 +0100

    WIP #37858 Refactoring (identical code in edit and show views for projects)
    Change-Id: Id5b4dc5244285a3a23c0c2179581108c99031492

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,25 @@
 class ProjectsController < ApplicationController
 
-	before_action :setup_project, except: [:index, :new, :create]
-	layout "show_project", only: [:show]
+  before_action :setup_project, except: [:index, :new, :create]
+  layout "show_project", only: [:show, :edit]
 
   def index
     @projects = Project.all
   end
 
-	def show
-		select_hash = {"desc" => ["Description", @project.description]}
-		tag = params[:tag]
-		@section_title = tag ? select_hash[tag][0] : nil
-		@content = tag ? select_hash[tag][1] : nil
-	end
+  def show
+    select_hash = {"desc" => ["Description", @project.description]}
+    tag = params[:tag]
+    @section_title = select_hash[tag][0] if select_hash[tag]
+    @content = select_hash[tag][1] if select_hash[tag]
+  end
 
   def new
     @project = Project.new
   end
 
-	def edit
-	end
+  def edit
+  end
 
   def create
     @project = Project.new(project_params)
@@ -32,13 +32,13 @@ class ProjectsController < ApplicationController
 
   end
 
-	def update
-		if @project.update(project_params)
-			redirect_to @project
-		else
-			render 'edit'
-		end
-	end
+  def update
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
 
   private
 
