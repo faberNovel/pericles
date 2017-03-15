aa4581c6015e84ffb037602ab8f0b83d7148738d
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-03-15 17:02:29 +0100

    WIP #39583 Adding create action for resources controller
    Change-Id: Iff2ab033b1c07a073a0aca937998dd4391c5bdbb

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class ResourcesController < ApplicationController
   layout 'show_project'
-  before_action :setup_project, only: [:index, :new]
+  before_action :setup_project, only: [:index, :new, :create]
 
   def index
     @resources = @project.resources
@@ -8,13 +8,35 @@ class ResourcesController < ApplicationController
 
   def new
     @resource = @project.resources.build
-    @selectable_resources = @project.resources
-    @selectable_resources.delete(@resource)
+    setup_selectable_resources(@project, @resource)
+  end
+
+  def create
+    @resource = @project.resources.build(resource_params)
+    if @resource.save
+      #FIXME
+      #redirect_to project_resource_path(@project, @resource)
+      redirect_to project_resources_path(@project)
+    else
+      setup_selectable_resources(@project, @resource)
+      render 'new'
+    end
   end
 
   private
 
   def setup_project
     @project = Project.includes(:resources).find(params[:project_id])
+  end
+
+  def setup_selectable_resources(project, resource)
+    @selectable_resources = project.resources
+    @selectable_resources.delete(resource)
+  end
+
+  def resource_params
+    params.require(:resource).permit(:name, :description,
+      resource_attributes_attributes: [:id, :name, :description, :primitive_type, :resource_id, :is_array, :example, :_destroy],
+      routes_attributes: [:id, :name, :description, :http_method, :url, :_destroy])
   end
 end
--- test/controllers/resources_controller_test.rb
+++ test/controllers/resources_controller_test.rb
@@ -14,4 +14,24 @@ class ResourcesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_not assigns[:selectable_resources].include?(assigns[:resource])
   end
+
+  test "should create resource" do
+    resource = build(:resource)
+    assert_difference('Resource.count') do
+      post project_resources_path(resource.project), params: { resource: resource.attributes }
+    end
+    resource = assigns[:resource]
+    assert_not_nil resource, "should create resource"
+    #FIXME
+    #assert_redirected_to project_resource_path(resource.project, resource)
+  end
+
+  test "should not create resource without a name" do
+    resource = build(:resource)
+    resource.name = ""
+    assert_no_difference('Resource.count') do
+      post project_resources_path(resource.project), params: { resource: resource.attributes }
+    end
+    assert_response :success
+  end
 end
