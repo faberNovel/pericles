60d8ab3dbc6c815c3bc5af55fedf02ad2602b456
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-03-22 15:26:23 +0100

    Routing errors are now handled
    Change-Id: I9612491060a68aa7bad07b96fae7f865414c9def

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
   match "/not_found", to: "errors#not_found", via: :all
   root "projects#index"
-
+  match '*path', to: "errors#not_found", via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
