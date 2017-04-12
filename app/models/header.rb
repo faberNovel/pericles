f1700942fb897829a5ddec6d2a4c9d9ffc6554fb
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-04-12 16:09:21 +0200

    WIP #40047 Adding inverse_of option so that headers can be created with responses
    Change-Id: I671b753dccc828eab3ade54533121b54a7bca3f3

--- app/models/header.rb
+++ app/models/header.rb
@@ -4,6 +4,5 @@ class Header < ApplicationRecord
   #FIXME validation passes when two headers with the same name, nested in same http_message
   # are created at the same time
   validates :name, presence: true, uniqueness: { scope: :http_message, case_sensitive: false }
-  #FIXME Cannot create headers and the parent http_message at the same time
   validates :http_message, presence: true
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ApplicationRecord
   belongs_to :route, inverse_of: :responses
 
-  has_many :headers, as: :http_message, dependent: :destroy
+  has_many :headers, inverse_of: :http_message, as: :http_message, dependent: :destroy
 
   accepts_nested_attributes_for :headers, allow_destroy: true, reject_if: :all_blank
 
