d1ba89dfd5877f8c9082f44cb102ee6c715df156
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-04-13 17:09:24 +0200

    WIP #40047 Removing uniqueness constraint for name within http_messages in Header model
    Change-Id: I819c1e788ee69be95449b627eb433412cf923900

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,8 +1,6 @@
 class Header < ApplicationRecord
   belongs_to :http_message, polymorphic: true
 
-  #FIXME validation passes when two headers with the same name, nested in same http_message
-  # are created at the same time
-  validates :name, presence: true, uniqueness: { scope: :http_message, case_sensitive: false }
+  validates :name, presence: true
   validates :http_message, presence: true
 end
--- test/models/header_test.rb
+++ test/models/header_test.rb
@@ -9,14 +9,6 @@ class HeaderTest < ActiveSupport::TestCase
     assert_not build(:header, http_message: nil).valid?
   end
 
-  test "Two headers within the same http_message shouldn't have the same name" do
-    header = create(:header)
-    name = header.name
-    http_message = header.http_message
-    assert_not build(:header, name: name, http_message: http_message).valid?
-    assert_not build(:header, name: name.upcase, http_message: http_message).valid?
-  end
-
   test "Header should be valid with all attributes set correctly" do
     assert build(:header, name: "Content-Type", description: "New test header").valid?
   end
