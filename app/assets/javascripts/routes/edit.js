010f2d74509e8f0ddb52d3d69f91293228ed8d58
Emilie Paillous <emilie.paillous@applidium.com>
2017-04-25 18:56:39 +0200

    WIP generate json schema : update json if it was already setted
    Change-Id: I25de1790f8e56059f088a185d6b156a3493ae548

--- app/assets/javascripts/routes/edit.js
+++ app/assets/javascripts/routes/edit.js
@@ -7,7 +7,37 @@ $(document).ready(function() {
         dataType: 'json'
       })
       .done(function(data) {
-        $('#json_schema_from_resource_text_area').text(JSON.stringify(data, null, 2));
+        var result_json = {};
+        var original_data = $('#json_schema_from_resource_text_area').text();
+        if (original_data) {
+          original_json = JSON.parse(original_data);
+          result_json = deep_merge(original_json, data);
+        } else {
+          result_json = data;
+        }
+        $('#json_schema_from_resource_text_area').text(JSON.stringify(result_json, null, 2));
       });
   });
 });
+
+
+function deep_merge(first_object, last_object) {
+  var result = first_object;
+  for (var key in first_object) {
+    if (last_object[key] && isObject(last_object[key])) {
+      result[key] = deep_merge(first_object[key], last_object[key]);
+    } else {
+      result[key] = first_object[key];
+    }
+  }
+  for (var key_last_object in last_object) {
+    if (!result[key_last_object]) {
+      result[key_last_object] = last_object[key_last_object];
+    }
+  }
+  return result;
+}
+
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
