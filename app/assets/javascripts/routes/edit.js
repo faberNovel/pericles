0521375051322ca5e68e3d869b4bfbbd8b8591ed
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-05-31 12:11:15 +0200

    WIP #41224 Generate schema from resource button now works for existing routes
    Change-Id: I9daa564d1f2667bab8bded34bf4ac2fa33242c56

--- app/assets/javascripts/routes/edit.js
+++ app/assets/javascripts/routes/edit.js
@@ -1,38 +1,48 @@
 $(document).ready(function() {
-  var url = window.location.pathname.replace('/edit', '.json_schema');
-  $("#generate_json_schema_from_resource").on("click", function() {
-    $.ajax({
-        type: "GET",
-        url: url,
-        dataType: 'json'
-      })
-      .done(function(data) {
-        var result_json = {};
-        var original_data = $('#json_schema_from_resource_text_area').text();
-        if (original_data) {
-          original_json = JSON.parse(original_data);
-          result_json = deep_merge(original_json, data);
-        } else {
-          result_json = data;
-        }
-        $('#json_schema_from_resource_text_area').text(JSON.stringify(result_json, null, 2));
-      });
-  });
-
-  $("#route_responses").children(".fields").each(set_id_response_headers_table);
+  $("#route_responses").children(".fields").each(set_id_response_element);
 
   $(this).on('nested:fieldAdded:responses', function(event) {
-    var field = event.field;
-    set_id_response_headers_table(0, field);
+    var field_element = event.field.get(0);
+    set_id_response_element(0, field_element);
   });
 
 });
 
-function set_id_response_headers_table(index, element) {
-  var response_number = $(element).find(".form-group:first label").attr("for").match(/\d+/)[0];
+function get_response_number(response_element) {
+  var response_number = $(response_element).find(".form-group:first label").attr("for").match(/\d+/)[0];
+  return response_number;
+}
+
+function set_id_response_element(index, element) {
+  var response_number = get_response_number(element);
+  set_id_response_headers_table(element, response_number);
+}
+
+function set_id_response_headers_table(element, response_number) {
   var response_table_headers_id = "table_response_" + response_number + "_headers";
   $(element).find("table").attr("id", response_table_headers_id);
   $(element).find("a.add_nested_fields").attr("data-target", "#" + response_table_headers_id);
+}
+
+function generate_schema_from_resource(clicked_button) {
+  var url = window.location.pathname.replace('/edit', '.json_schema');
+  var body_json_schema = $(clicked_button).siblings(".form-group").children("textarea");
+  $.ajax({
+      type: "GET",
+      url: url,
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var result_json = {};
+      var original_data = body_json_schema.text();
+      if (original_data) {
+        original_json = JSON.parse(original_data);
+        result_json = deep_merge(original_json, data);
+      } else {
+        result_json = data;
+      }
+      body_json_schema.text(JSON.stringify(result_json, null, 2));
+    });
 }
 
 function deep_merge(first_object, last_object) {
