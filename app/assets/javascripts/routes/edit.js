b63342906fd96e3562db38a08deffbe907514b70
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-06-01 19:22:05 +0200

    FIX #41579 There are now suggestions for headers
    Change-Id: I4ff95a764e21c73fceb4aea1a4e9408b0c183789

--- app/assets/javascripts/routes/edit.js
+++ app/assets/javascripts/routes/edit.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
     set_id_response_element(0, field_element);
   });
 
+  $('input').filter(function() {
+    return this.id.match(/headers_attributes_\d+_name$/) !== null;
+  }).each(set_autocomplete);
+
+  $(this).on('nested:fieldAdded:request_headers nested:fieldAdded:headers', function(event) {
+    var input_element = event.field.find(".form-group:first input").get(0);
+    set_autocomplete(0, input_element);
+  });
+
 });
 
 function get_response_number(response_element) {
@@ -43,6 +52,25 @@ function generate_schema_from_resource(clicked_button) {
       }
       body_json_schema.text(JSON.stringify(result_json, null, 2));
     });
+}
+
+function set_autocomplete(index, element) {
+  $(element).autocomplete({
+    source: function(request, response) {
+      jQuery.get("/headers", {
+        term: request.term
+      }, function(data) {
+        var field_name_array = data.headers.map(function(header) {
+          return header.name;
+        });
+        response(field_name_array);
+      });
+    },
+    messages: {
+      noResults: '',
+      results: function() {}
+    }
+  });
 }
 
 function deep_merge(first_object, last_object) {
