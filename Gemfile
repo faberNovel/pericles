fb97dbfafbd94cd511ba037793f08a1c7bd2470b
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-06-01 19:21:29 +0200

    WIP #41579 Integrating jquery-ui-rails gem
    Change-Id: I21b11eeaa6ecc094666bc6330c721919d07dbdfb

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'rubyzip'
 
 gem 'ransack', github: 'activerecord-hackery/ransack'
 
+gem 'jquery-ui-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery_nested_form
+//= require jquery-ui/widgets/autocomplete
 //= require turbolinks
 //= require bootstrap-sprockets
