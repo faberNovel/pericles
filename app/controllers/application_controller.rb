da75adcb42fd31964abf11d8110aa3c6a082444c
Hugo Hache <hugo.hache@applidium.com>
2017-07-05 14:36:59 +0200

    Adding http auth enabled only if dedicated secret present
    Change-Id: I12ac749f6d312e5e494e405cc4a9e7a45e5d9232

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  if Rails.application.secrets.http_auth_user
+    http_basic_authenticate_with name: Rails.application.secrets.http_auth_user, password: Rails.application.secrets.http_auth_password
+  end
+
   rescue_from ActiveRecord::RecordNotFound do
     redirect_to "/not_found"
   end
