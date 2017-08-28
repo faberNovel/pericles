0705247207f6eb0bc45d97fbe0032f00262a1c4e
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-08-28 17:29:09 +0200

    WIP #42867 Adding custom_nullable field to attributes_resource_representations table
    Change-Id: Iead5db1c0c00d2596b29158c6d915d92413ca42b

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720153921) do
+ActiveRecord::Schema.define(version: 20170828152358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170720153921) do
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
     t.integer  "resource_representation_id"
+    t.boolean  "custom_nullable"
     t.index ["attribute_id"], name: "index_attributes_resource_representations_on_attribute_id", using: :btree
     t.index ["parent_resource_representation_id"], name: "index_arr_on_parent_resource_representation_id", using: :btree
     t.index ["resource_representation_id"], name: "index_arr_on_resource_representation_id", using: :btree
