c80a62fb28ddbaf514d3c0f80e890f6280a5ab01
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-09-01 15:42:59 +0200

    WIP #41337 Adding name (first_name and last_name) to users
    Change-Id: Iaea5f62b15e5e1f61f757f21ffef9a09e1d999e5

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,13 @@ class User < ApplicationRecord
     data = access_token.info
 
     User.find_or_create_by(email: data['email']) do |user|
+      user.first_name = data['first_name']
+      user.last_name = data['last_name']
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def name
+    "#{first_name} #{last_name}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830101250) do
+ActiveRecord::Schema.define(version: 20170901114536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,8 @@ ActiveRecord::Schema.define(version: 20170830101250) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
