696526ac8864d34171fe34564d95c7b9c1bdd5bd
Charles-Elie Simon <charles-elie.simon@applidium.com>
2017-09-04 14:37:31 +0200

    WIP #41337 Removing respond_to in index of headers_controller
    Change-Id: Ifcb5ec2a2e47a3d04834514da3ce09e61abdad83

--- app/controllers/headers_controller.rb
+++ app/controllers/headers_controller.rb
@@ -1,11 +1,7 @@
 class HeadersController < ApplicationController
   def index
-    respond_to do |format|
-      format.json do
-        term = params[:term] ? params[:term] : ""
-        @found_headers = Header.ransack(name_cont: term).result(distinct: true).select(:name)
-        render json: @found_headers, status: :ok
-      end
-    end
+    term = params[:term] ? params[:term] : ""
+    @found_headers = Header.ransack(name_cont: term).result(distinct: true).select(:name)
+    render json: @found_headers, status: :ok
   end
 end
