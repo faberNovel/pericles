8610ee0a9ed931e3e7726c73775513b475ee6499
Clément Villain <clement.villain@applidium.com>
2017-09-18 17:54:22 +0200

    Keep GenerateJsonInstanceService context in memory instead of loading it for every request
    Change-Id: I0a3fc92b67494ad7a61de04c7c3e3b1e29716da8

--- app/models/services/generate_json_instance_service.rb
+++ app/models/services/generate_json_instance_service.rb
@@ -1,11 +1,13 @@
 class GenerateJsonInstanceService
+  source = File.open(File.join(Rails.root, 'app', 'assets', 'javascripts', 'json-schema-faker.min.js')).read
+  # Avoid reloading the context, we save ~500ms/request
+  @@context = ExecJS.compile(source)
+
   def initialize(schema)
     @schema = schema
   end
 
   def execute
-    source = File.open(File.join(Rails.root, 'app', 'assets', 'javascripts', 'json-schema-faker.min.js')).read
-    context = ExecJS.compile(source)
-    context.eval("jsf(" + @schema.to_json + ")");
+    @@context.eval("jsf(" + @schema.to_json + ")");
   end
 end
