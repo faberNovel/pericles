709429ab7d42a1c21ca85b9dfe497048d1dc48de
Clément Villain <clement.villain@applidium.com>
2017-09-18 15:57:39 +0200

    Fix #43315: Display last_edit with last_audit
    in route, representation and resource
    
    Change-Id: I3964a92df2b4807baa0056d5e75fdc0eea47cdfd

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def last_audit
+    (audits.to_a + associated_audits.to_a).max_by { |audit| audit.created_at }
+  end
 end
