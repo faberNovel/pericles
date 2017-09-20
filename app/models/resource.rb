d6d427e5b6ceb4c1bdf70940cfcbfe8ecb688d95
Clément Villain <clement.villain@applidium.com>
2017-09-20 11:47:37 +0200

    Fix #42091: add a default representation to attribute.resource
    If we add a new attribute like user: User we need to make sure
    that the representation of user use a representation for User
    
    Change-Id: I11f8bbe0be8d7e382770ff0770aec3543ebe8e54

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -19,7 +19,12 @@ class Resource < ApplicationRecord
     resource_representation = self.resource_representations.create(name: "default_representation",
       description: "Automatically generated")
     self.resource_attributes.each do |attribute|
-      resource_representation.attributes_resource_representations.create(resource_attribute: attribute, is_required: true)
+      resource_referenced_by_attribute_representation = attribute.resource&.resource_representations&.first
+      resource_representation.attributes_resource_representations.create(
+        resource_attribute: attribute,
+        is_required: true,
+        resource_representation: resource_referenced_by_attribute_representation
+      )
     end
   end
 end
