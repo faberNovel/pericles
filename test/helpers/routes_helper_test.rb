d5a383d207dc8961b3c11c6d29f0a3fd99f2c3ba
Charles-Elie SIMON <charles-elie.simon@fabernovel.com>
2017-09-29 16:39:00 +0200

    Modifying how routes are displayed in show view of resources
    Change-Id: I065b7e76482918ea44148583d20a3a1b7fcdd0cb

--- test/helpers/routes_helper_test.rb
+++ test/helpers/routes_helper_test.rb
@@ -17,4 +17,20 @@ class RoutesHelperTest < ActionView::TestCase
   test "route_includes_json_schema should return false if route does not include a json_schema" do
     assert_equal false, route_includes_json_schema(@route)
   end
+
+  test "get_http_method_label_class" do
+    assert_equal "label label-success http-method-label", get_http_method_label_class(@route)
+
+    @route.update(http_method: :POST)
+    assert_equal "label label-warning http-method-label", get_http_method_label_class(@route)
+
+    @route.update(http_method: :PUT)
+    assert_equal "label label-primary http-method-label", get_http_method_label_class(@route)
+
+    @route.update(http_method: :PATCH)
+    assert_equal "label label-primary http-method-label", get_http_method_label_class(@route)
+
+    @route.update(http_method: :DELETE)
+    assert_equal "label label-danger http-method-label", get_http_method_label_class(@route)
+  end
 end
