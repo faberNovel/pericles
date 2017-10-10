1385ffc809bb09c0c5abce0d67baef5c6f64629c
Charles-Elie SIMON <charles-elie.simon@fabernovel.com>
2017-10-10 17:10:45 +0200

    JSON Validation is now integrated in panels in routes show view
    Change-Id: I85a460f756cf4e7d03238fc356c2a49a896d5ba9

--- app/assets/javascripts/validations/new.js
+++ app/assets/javascripts/validations/new.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#validate_json_instance").on("click", function() {
     var json_schema = $("#json_schema").val();
     var json_instance = $("#json_instance").val();
-    validate_json_instance(json_schema, json_instance, display_validation_result);
+    var display_result_element = $("#json-validation-result");
+    validate_json_instance(json_schema, json_instance, display_result_element);
   });
 });
