8227466b2581386a12eb11c7a2b7d151425689ea
Clément Villain <clement.villain@fabernovel.com>
2017-10-11 12:10:21 +0200

    Include errors by default in Reports
    Change-Id: I1b63b627c004a8742946da42e5e9f6abf39730fe

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,8 @@ class Report < ApplicationRecord
 
   has_many :validation_errors
 
+  default_scope { includes(:validation_errors) }
+
   validates :status_code, :body, :headers, presence: true
 
   def correct?
