8b0ccd3c14f376665c0ef6f230399f5e18f0b835
Clément Villain <clement.villain@fabernovel.com>
2017-10-13 14:27:34 +0200

    Update background color for reports
    Change-Id: Ieed49b830e682bb4d7832ea4a8bdd651fa0451d2

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -4,8 +4,8 @@ module RoutesHelper
     JSON.pretty_generate(JSON.parse(json))
   end
 
-  def get_http_method_label_class(route)
-    case route.http_method.to_sym
+  def label_class_for_http_method(http_method)
+    case http_method&.to_sym
     when :GET
       "label label-success http-method-label"
     when :POST
@@ -14,6 +14,8 @@ module RoutesHelper
       "label label-primary http-method-label"
     when :PATCH
       "label label-primary http-method-label"
+    when :OPTIONS
+      "label label-warning http-method-label"
     when :DELETE
       "label label-danger http-method-label"
     end
