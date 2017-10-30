616b8b48f8c0aa13aa08194c6744437c955964fe
Clément Villain <clement.villain@fabernovel.com>
2017-10-30 15:28:01 +0100

    Add active class to top navbar
    Fix #44028
    
    Change-Id: I9045ea1c9166813a6334ea88ed1307849d3ff8d0

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active_class(controller_class)
     controller_class == controller.class ? 'active' : ''
   end
+
+  def top_nav_active_class(path)
+    return request.url.include?(path) ? 'active' : ''
+  end
 end
