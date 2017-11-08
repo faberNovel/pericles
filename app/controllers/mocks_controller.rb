c767c1df67e3fd1e9aaaaa9d6d9d4efe986a0789
Clément Villain <clement.villain@fabernovel.com>
2017-11-08 15:36:23 +0100

    Render status_code correctly
    Change-Id: I2bfde5f9f6e905c9d810872c331bd9be9e1e6ba3

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -7,11 +7,14 @@ class MocksController < ApplicationController
       route = Route.find_by_id(main_route[:name])
       if route.active_mock
         mock_body = route.active_mock.body
+        status_code = route.active_mock.response.status_code
       else
-        schema = route.responses.first.json_schema
+        response = route.responses.first
+        schema = response.json_schema
+        status_code = response.status_code
         mock_body = GenerateJsonInstanceService.new(schema).execute
       end
-      render json: mock_body
+      render json: mock_body, status: status_code
     else
       render json: {error: 'Route not found'}, status: :not_found
     end
