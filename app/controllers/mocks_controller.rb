acbb1cf611ebb44211da3406f2a1929f6b321b17
Clément Villain <clement.villain@fabernovel.com>
2017-11-08 15:19:17 +0100

    Use active mock if present
    Change-Id: If5f008eb4f91322d3020724202dabdd9f8e76392

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -4,8 +4,14 @@ class MocksController < ApplicationController
     routes = Project.find(params[:project_id]).build_route_set
     main_route = routes.recognize_path('/' + params[:path], { method: request.method })
     if main_route
-      schema = Route.find_by_id(main_route[:name]).responses.first.json_schema
-      render json: GenerateJsonInstanceService.new(schema).execute
+      route = Route.find_by_id(main_route[:name])
+      if route.active_mock
+        mock_body = route.active_mock.body
+      else
+        schema = route.responses.first.json_schema
+        mock_body = GenerateJsonInstanceService.new(schema).execute
+      end
+      render json: mock_body
     else
       render json: {error: 'Route not found'}, status: :not_found
     end
