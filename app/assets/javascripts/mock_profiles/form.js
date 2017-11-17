605204676859e81e17e5414e56e71ff6951b9800
Clément Villain <clement.villain@fabernovel.com>
2017-11-17 16:27:37 +0100

    Display correct select when adding new field
    Change-Id: I39ebff9cb155de3f6d7067fb26baa54ea10c5e8d

--- app/assets/javascripts/mock_profiles/form.js
+++ app/assets/javascripts/mock_profiles/form.js
@@ -1,8 +1,17 @@
+function hideSelect() {
+  $(".error-response .resource-instances").parents(".form-group").hide();
+  $(".resource-response .api-error-instances").parents(".form-group").hide();
+  $('.chosen-container').css('width', 'auto');
+  $('select').chosen();
+}
+
 $(document).on('nested:fieldAdded', function(event){
   var field = event.field;
   var responseField = field.find('.response-id');
 
   var value = responseField.parents('.response').attr('data-response-id')
   responseField.val(value);
-  $('select').chosen();
-})
+  hideSelect();
+});
+
+$(document).ready(hideSelect);
