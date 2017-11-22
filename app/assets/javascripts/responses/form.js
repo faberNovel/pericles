d38bb19cfbf770f6dfca90b5ef0f0c4bf6e85bf2
Clément Villain <clement.villain@fabernovel.com>
2017-11-22 10:15:04 +0100

    Hide resource representation and api error select if status_code is not defined
    Change-Id: Ibe05c4316965eba1cf8ca8c972a22e0960a8460f

--- app/assets/javascripts/responses/form.js
+++ app/assets/javascripts/responses/form.js
@@ -1,16 +1,23 @@
-$(document).ready(function () {
-  $('#response_status_code').on('change', function () {
-    if ($('#response_status_code').val() >= 400) {
-      $('#response_resource_representation_id').val('').trigger('chosen:updated');
-      $('#response_resource_representation_id').parent().hide();
-      $('#response_api_error_id').parent().show()
-    } else {
-      $('#response_api_error_id').val('').trigger('chosen:updated');
-      $('#response_api_error_id').parent().hide();
-      $('#response_resource_representation_id').parent().show()
-    }
-  });
+function updateOnStatusChange () {
+  if (!$('#response_status_code').val()) {
+    $('#response_api_error_id').parent().hide();
+    $('#response_resource_representation_id').parent().hide();
+    return;
+  }
 
-  $('.resource-representation #response_api_error_id').parent().hide();
-  $('.api-error #response_resource_representation_id').parent().hide();
+  if ($('#response_status_code').val() >= 400) {
+    $('#response_resource_representation_id').val('').trigger('chosen:updated');
+    $('#response_resource_representation_id').parent().hide();
+    $('#response_api_error_id').parent().show()
+  } else {
+    $('#response_api_error_id').val('').trigger('chosen:updated');
+    $('#response_api_error_id').parent().hide();
+    $('#response_resource_representation_id').parent().show()
+  }
+}
+
+$(document).ready(function () {
+  $('#response_status_code').on('change', updateOnStatusChange);
+
+  updateOnStatusChange();
 });
