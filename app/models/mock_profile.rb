c6aaf64bda180144bfb270e16172e3650c7b702c
Clément Villain <clement.villain@fabernovel.com>
2017-12-12 08:27:19 +0100

    Add ancestry to mock_profiles
    Change-Id: Ib25120124ae2e163d7b78a19fe77949585980ba8

--- app/models/mock_profile.rb
+++ app/models/mock_profile.rb
@@ -2,6 +2,7 @@ class MockProfile < ApplicationRecord
   belongs_to :project
   has_many :mock_pickers
   has_many :responses, through: :mock_pickers
+  has_ancestry
 
   accepts_nested_attributes_for :mock_pickers, allow_destroy: true
 
