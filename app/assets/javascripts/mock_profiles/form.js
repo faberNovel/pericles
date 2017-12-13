88a76edaa0527a5e622e5a99214b94915cd8ec14
Clément Villain <clement.villain@fabernovel.com>
2017-12-13 16:30:23 +0100

    Fix body pattern and url pattern not updated and add error message
    Change-Id: I1f423de64cffd165835f8bf2f6496f63e2763759

--- app/assets/javascripts/mock_profiles/form.js
+++ app/assets/javascripts/mock_profiles/form.js
@@ -7,6 +7,20 @@ $(document).ready(function() {
     div['id'] = uniqid;
     var a = $(added_element[0]).children('a[href="#-advanced"]')[0];
     a['href'] = "#" + uniqid;
+
+
+    // Regexp field need javascript listeners so if we add some we need to add back the listeners
+    // TODO: Clément Villain 13/12/17 move this code in a function in regex_field gem
+    $('#' + uniqid + ' .regex').on('focus', function() {
+      this.innerHTML = this.textContent || this.innerText;
+    });
+
+    $('#' + uniqid + ' .regex').on('blur', function() {
+      RegexColorizer.colorizeAll();
+
+      target = $(this).data('target');
+      $(this).parent().find("input[id$='" + target + "']").val(this.textContent || this.innerText);
+    });
   })
 
   $('#mock_profile_form').on('cocoon:before-remove', function(event, removed_element) {
