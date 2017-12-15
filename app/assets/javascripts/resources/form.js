35c997e9cf707f1eead8efb8b17bdd205e21c7bc
Clément Villain <clement.villain@fabernovel.com>
2017-12-15 18:59:18 +0100

    Hide or show enum and scheme
    Change-Id: I7d1b1d07c05251189baddbef08120595bb948596

--- app/assets/javascripts/resources/form.js
+++ app/assets/javascripts/resources/form.js
@@ -1,12 +1,23 @@
 function onSelectChanged(option) {
   var minimum = $(option).parents('.fields').find(".constraints input[id$='minimum']").parents('.form-group');
   var maximum = $(option).parents('.fields').find(".constraints input[id$='maximum']").parents('.form-group');
+  var scheme = $(option).parents('.fields').find(".constraints select[id$='scheme_id']").parents('.form-group');
+  var enum_ = $(option).parents('.fields').find(".constraints input[id$='enum']").parents('.form-group');
+
   if (option.value === 'integer' || option.value == 'string') {
     minimum.show();
     maximum.show();
   } else {
     minimum.hide();
     maximum.hide();
+  }
+
+  if (option.value === 'string') {
+    scheme.show();
+    enum_.show();
+  } else {
+    scheme.hide();
+    enum_.hide();
   }
 }
 
