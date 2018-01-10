7471b5e328d722f2d019a59bc9ff6b9f4a08088f
Clément Villain <clement.villain@fabernovel.com>
2018-01-10 16:40:54 +0100

    Reduce the frustration of runninng a controller test to 0
    Change-Id: I850c280e500d8ea757a266b7353a4fccb3d818d9

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ Minitest::Reporters.use!
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'authentication/controller_with_authentication_test.rb'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
