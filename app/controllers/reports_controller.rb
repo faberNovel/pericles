09970e6a33c5a1b14b3c5499038140be51e1f426
Clément Villain <clement.villain@fabernovel.com>
2018-01-10 16:48:18 +0100

    Add Authenticated and ProjectRelated to ReportsController
    Change-Id: Ib36e59999ceae724c8517ce565e693b96f16f58c

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,20 +1,15 @@
 class ReportsController < ApplicationController
+  include ProjectRelated
+  include Authenticated
+
   layout 'full_width_column'
   decorates_assigned :report
 
   def index
-    @project = project
-    @reports = @project.reports.order(created_at: :desc).page params[:page]
+    @reports = project.reports.order(created_at: :desc).page params[:page]
   end
 
   def show
-    @project = project
-    @report = @project.reports.find(params[:id])
-  end
-
-  private
-
-  def project
-    Project.find(params[:project_id])
+    @report = project.reports.find(params[:id])
   end
 end
--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ReportsControllerTest < ActionDispatch::IntegrationTest
+class ReportsControllerTest < ControllerWithAuthenticationTest
   setup do
     @project = create(:full_project)
     @report = create(:report, route: @project.routes.first, project: @project)
@@ -14,5 +14,17 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get project_report_path(@project, @report)
     assert_response :success
+  end
+
+  test "user must be logged in to get index" do
+    sign_out :user
+    get project_reports_path(@project)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "user must be logged in to get show" do
+    sign_out :user
+    get project_report_path(@project, @report)
+    assert_redirected_to new_user_session_path
   end
 end
