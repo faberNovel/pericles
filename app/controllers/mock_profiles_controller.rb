100ee711e3cc0f8d203f49811e0ff032c7754492
Clément Villain <clement.villain@fabernovel.com>
2018-01-15 11:21:58 +0100

    Use lazy for MockProfilesController
    Change-Id: Ib7812ac3dc8e9edeee61c88770cda654a59a46a5

--- app/controllers/mock_profiles_controller.rb
+++ app/controllers/mock_profiles_controller.rb
@@ -2,21 +2,17 @@ class MockProfilesController < MocksController
   include ProjectRelated
 
   layout 'full_width_column'
-  before_action :setup_mock_profile, only: [:edit, :update]
+  lazy_controller_of :mock_profile, helper_method: true
 
   def index
     @mock_profiles = project.mock_profiles
   end
 
   def new
-    @mock_profile = project.mock_profiles.new
-    authorize @mock_profile
   end
 
   def create
-    @mock_profile = project.mock_profiles.new(mock_profile_params)
-    authorize @mock_profile
-    if @mock_profile.save
+    if mock_profile.save
       redirect_to_mock_profile
     else
       render 'new', layout: 'full_width_column', status: :unprocessable_entity
@@ -27,45 +23,34 @@ class MockProfilesController < MocksController
   end
 
   def update
-    if @mock_profile.update(mock_profile_params)
+    if mock_profile.update(permitted_attributes(mock_profile))
       redirect_to_mock_profile
     else
       render 'edit', layout: 'full_width_column', status: :unprocessable_entity
     end
   end
 
-  def pick_profile
-    MockProfile.find(params[:mock_profile_id])
-  end
-
   private
 
-  def setup_mock_profile
-    @mock_profile = MockProfile.includes(:mock_pickers).find(params[:id])
-    authorize @mock_profile
-    @mock_pickers_by_response_id = @mock_profile.mock_pickers.includes(:resource_instances, :api_error_instances).group_by(&:response_id)
-    @project = @mock_profile.project
+  def find_project
+    # With find_mock_profile we avoid authorization process that create an infinite loop
+    super || find_mock_profile.project
   end
 
-  def mock_profile_params
-    params.require(:mock_profile).permit(
-      :name,
-      :parent_id,
-      mock_pickers_attributes: [
-        :id,
-        :body_pattern,
-        :url_pattern,
-        :resource_instance_ids,
-        :api_error_instance_ids,
-        :response_id,
-        :_destroy,
-        resource_instance_ids: [],
-        api_error_instance_ids: [],
-      ]
-    )
+  def build_mock_profile_from_params
+    project.mock_profiles.new(permitted_attributes(MockProfile)) if params.has_key? :mock_profile
   end
+
+  def new_mock_profile
+    project.mock_profiles.new
+  end
+
+  def mock_pickers_by_response_id
+    @mock_pickers_by_response_id ||= mock_profile.mock_pickers.includes(:resource_instances, :api_error_instances).group_by(&:response_id)
+  end
+  helper_method :mock_pickers_by_response_id
 
   def redirect_to_mock_profile
-    redirect_to edit_mock_profile_path(@mock_profile)
+    redirect_to edit_mock_profile_path(mock_profile)
   end
 end
--- app/policies/mock_profile_policy.rb
+++ app/policies/mock_profile_policy.rb
@@ -2,4 +2,22 @@ class MockProfilePolicy < ProjectRelatedPolicy
   def edit?
     show?
   end
+
+  def permitted_attributes
+    [
+      :name,
+      :parent_id,
+      mock_pickers_attributes: [
+        :id,
+        :body_pattern,
+        :url_pattern,
+        :resource_instance_ids,
+        :api_error_instance_ids,
+        :response_id,
+        :_destroy,
+        resource_instance_ids: [],
+        api_error_instance_ids: [],
+      ]
+    ]
+  end
 end
