2c9493e206d0b8b95df0516ce88bb1f9fbc5e9d7
Clément Villain <clement.villain@fabernovel.com>
2018-01-15 09:58:07 +0100

    Use lazy pattern for ResourcesController
    Change-Id: I62df8378455d6a1bf5c47fac760a2ca832fec09e

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,9 +1,8 @@
 class ResourcesController < ApplicationController
   include ProjectRelated
-
   layout 'full_width_column'
-  before_action :setup_resource, except: [:index, :new, :create]
-  decorates_assigned :resource
+  lazy_controller_of :resource
+  lazy_decorates_assigned :resource
 
   def index
     @resources = project.resources.sort_by { |resource| resource.name.downcase }
@@ -21,41 +20,33 @@ class ResourcesController < ApplicationController
   end
 
   def new
-    @resource = project.resources.build
-    authorize @resource
-    setup_types
   end
 
   def edit
-    setup_types
   end
 
   def create
-    @resource = project.resources.build(resource_params)
-    authorize @resource
     check_valid_json_object_param(params[:json_instance]) unless params[:json_instance].blank?
-    if @json_instance_error.blank? && @resource.save
-      @resource.try_create_attributes_from_json(params[:json_instance]) if params[:json_instance]
-      redirect_to project_resource_path(project, @resource)
+    if @json_instance_error.blank? && resource.save
+      resource.try_create_attributes_from_json(params[:json_instance]) if params[:json_instance]
+      redirect_to project_resource_path(project, resource)
     else
-      setup_types
       @json_instance = params[:json_instance]
       render 'new', status: :unprocessable_entity
     end
   end
 
   def update
-    if @resource.update(resource_params)
-      redirect_to project_resource_path(project, @resource)
+    if resource.update(resource_params)
+      redirect_to project_resource_path(project, resource)
     else
-      setup_types
       render 'edit', status: :unprocessable_entity
     end
   end
 
   def destroy
     begin
-      @resource.destroy
+      resource.destroy
       redirect_to project_resources_path(project)
     rescue ActiveRecord::InvalidForeignKey
       flash.now[:error] = t('activerecord.errors.models.resource.attributes.base.destroy_failed_foreign_key')
@@ -65,10 +56,16 @@ class ResourcesController < ApplicationController
 
   private
 
-  def setup_resource
-    @resource = project.resources.find(params[:id])
-    authorize @resource
-    @resource_representations = @resource.resource_representations.order(:name)
+  def find_resource
+    project.resources.find(params[:id]) if params.has_key? :id
+  end
+
+  def build_resource_from_params
+    project.resources.build(resource_params) if params.has_key? :resource
+  end
+
+  def new_resource
+    project.resources.build
   end
 
   def check_valid_json_object_param(json_string)
@@ -122,10 +119,13 @@ class ResourcesController < ApplicationController
     @resource_params
   end
 
-  def setup_types
+  def available_types
+    return @available_types if defined? @available_types
+
     selectable_resources = project.resources.select(&:persisted?)
     primitive_types = Attribute.primitive_types.keys.to_a.map { |k| [k.capitalize, k]}
     resource_types = selectable_resources.sort_by(&:name).collect { |r| [ r.name, r.id ] }
-    @types = primitive_types + resource_types
+    @available_types = primitive_types + resource_types
   end
+  helper_method :available_types
 end
