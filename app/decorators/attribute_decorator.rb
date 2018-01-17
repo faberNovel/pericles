abbe0086ed92c6e337c81b28a8cae3426c0c4b92
Clément Villain <clement.villain@fabernovel.com>
2018-01-17 17:54:20 +0100

    Add constraints
    Change-Id: Icaa9df393e3022116d3ebe8ba1d62f1c41dc4176

--- app/decorators/attribute_decorator.rb
+++ app/decorators/attribute_decorator.rb
@@ -9,4 +9,23 @@ class AttributeDecorator < Draper::Decorator
     base_type = resource&.name || primitive_type.to_s.capitalize
     is_array ? "Array<#{base_type}>" : base_type
   end
+
+  def displayed_type
+    if resource_id
+      project = attribute.resource.project
+      h.link_to(readable_type, h.project_resource_path(project, attribute.resource))
+    else
+      readable_type
+    end
+  end
+
+  def constraints?
+    !(
+      attribute.enum.blank? &&
+      attribute.scheme.blank? &&
+      attribute.minimum.blank? &&
+      attribute.maximum.blank? &&
+      attribute.description.blank?
+    )
+  end
 end
