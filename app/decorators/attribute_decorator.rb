1aeeece836ebf0c61c200299a076345b58f7b8d1
Clément Villain <clement.villain@fabernovel.com>
2018-01-17 16:57:00 +0100

    Add static table
    Change-Id: If39bc72421cf4bbc8cb14f1b2307b5247f9b802f

--- app/decorators/attribute_decorator.rb
+++ app/decorators/attribute_decorator.rb
@@ -4,4 +4,9 @@ class AttributeDecorator < Draper::Decorator
   def key_name_placeholder
     name.underscore.parameterize(separator: '_')
   end
+
+  def readable_type
+    base_type = resource&.name || primitive_type.to_s.capitalize
+    is_array ? "Array<#{base_type}>" : base_type
+  end
 end
--- app/decorators/resource_decorator.rb
+++ app/decorators/resource_decorator.rb
@@ -5,7 +5,7 @@ class ResourceDecorator < Draper::Decorator
     # Note: Clément Villain 29/12/17
     # We do the sorting in ruby and not with active record
     # because we want to keep non persited objects
-    object.resource_attributes.sort_by(&:name)
+    object.resource_attributes.decorate.sort_by(&:name)
   end
 
   def resource_representations_with_color
