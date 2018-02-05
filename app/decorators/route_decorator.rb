fe531c6a25b6374dec33c8a65e2020482340a378
Clément Villain <clement.villain@fabernovel.com>
2018-02-05 16:35:28 +0100

    Add link to route summary
    Fix #44776
    
    Change-Id: Icf921755a207dd70b336e343083f6b8551156292

--- app/decorators/route_decorator.rb
+++ app/decorators/route_decorator.rb
@@ -4,4 +4,10 @@ class RouteDecorator < Draper::Decorator
   def responses
     object.responses.order(:status_code)
   end
+
+  def resource_representations_links
+    object.resource_representations.uniq.map do |r|
+      h.link_to(r.name, h.project_resource_path(r.project, r.resource_id, anchor: "res-#{r.id}"))
+    end.to_sentence.html_safe
+  end
 end
