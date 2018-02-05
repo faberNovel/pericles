60c7ae515891057df724ff9075ee751c1d12fca4
Clément Villain <clement.villain@fabernovel.com>
2018-02-05 10:09:43 +0100

    Add resource used in list to resource page
    Change-Id: Id40f67ac82d8f0bdc82cb2eb7bee385957f2ba1f

--- app/decorators/resource_decorator.rb
+++ app/decorators/resource_decorator.rb
@@ -13,4 +13,10 @@ class ResourceDecorator < Draper::Decorator
       [r, "color-#{i}"]
     end
   end
+
+  def used_in_resources_links
+    object.used_in_resources.map do |r|
+      h.link_to(r.name, h.project_resource_path(r.project, r))
+    end.to_sentence.html_safe
+  end
 end
