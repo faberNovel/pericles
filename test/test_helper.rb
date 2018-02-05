2b85cf0d409b31e5d624f864378c6d7dce79809b
Clément Villain <clement.villain@fabernovel.com>
2018-02-05 16:43:18 +0100

    Move fixtures in test folder
    Fix #44722
    
    Change-Id: I29bcdd1d7562cc457bb8e9cf5105d5ad59bf2e7f

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,6 +19,6 @@ class ActionDispatch::IntegrationTest
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "test/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
