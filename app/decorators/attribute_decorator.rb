f6b93884a2cd6f636441d69de94cff788356479f
Clément Villain <clement.villain@fabernovel.com>
2018-02-23 17:47:08 +0100

    Remove dead code
    Change-Id: I247ff01c298a444b19d846afe489cd23e7211c8c

--- app/decorators/attribute_decorator.rb
+++ app/decorators/attribute_decorator.rb
@@ -9,27 +9,4 @@ class AttributeDecorator < Draper::Decorator
     base_type = resource&.name || primitive_type.to_s.capitalize
     is_array ? "Array<#{base_type}>" : base_type
   end
-
-  def displayed_type
-    if resource_id
-      project = attribute.resource.project
-      h.link_to(
-        readable_type,
-        h.project_resource_path(project, attribute.resource),
-        'resource-name': readable_type
-      )
-    else
-      readable_type
-    end
-  end
-
-  def constraints?
-    !(
-      attribute.enum.blank? &&
-      attribute.scheme.blank? &&
-      attribute.minimum.blank? &&
-      attribute.maximum.blank? &&
-      attribute.description.blank?
-    )
-  end
 end
