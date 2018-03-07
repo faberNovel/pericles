a6f59d5fc450dc9ffc95e37af58d565fa800b1ca
Clément Villain <clement.villain@fabernovel.com>
2018-03-07 16:21:07 +0100

    Space Projects/Schemes/JSON Validation
    Change-Id: I90fa420287a90fd64c990b10095cff3a7c4eacbb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def active_class(controller_class)
-    controller_class == controller.class ? 'active' : ''
-  end
-
-  def top_nav_active_class(path)
-    return request.url.include?(path) ? 'active' : ''
-  end
 end
