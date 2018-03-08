cce5052f06b243dcb9066e20c2967ee5418ea839
Clément Villain <clement.villain@fabernovel.com>
2018-03-08 14:37:07 +0100

    Implement search methods
    Change-Id: I9f601dfdde0c6e1cb73adb23c6317ad41a2f5020

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -4,11 +4,18 @@ class SearchService
   end
 
   def search(query)
+    return [] if query.blank?
+
+    # TODO Clément Villain 15/03/2018
+    # Refactor search_* methods using pg_search gem or
+    # Postgres Basic Text Matching
+
     [
       search_api_error_instances(query),
       search_api_errors(query),
       search_attributes(query),
-      search_headers(query),
+      search_response_headers(query),
+      search_request_headers(query),
       search_metadata(query),
       search_mock_pickers(query),
       search_mock_profiles(query),
@@ -20,104 +27,212 @@ class SearchService
       search_resources(query),
       search_responses(query),
       search_routes(query),
-      search_description(query),
       search_schemes(query),
       search_validation_errors(query)
     ].sum
   end
 
-  def search_api_error_instances(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
+  def search_api_error_instances(query)
+    api_error_instances = ApiErrorInstance.joins(:api_error).where(
+      api_errors: { project: @project }
+    )
 
-  def search_api_errors(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_attributes(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_headers(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_metadata(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_mock_pickers(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_mock_profiles(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_project(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_query_parameters(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_reports(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_resource_instances(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_resource_representations(_query)
-    # TODO Clément Villain 8/03/18
-    []
-  end
-
-  def search_resources(query)
-    @project.resources.where(
-      "name like ?", "%#{query}%"
+    api_error_instances.where(
+      "api_error_instances.name like ?", "%#{query}%"
     ).or(
-      @project.resources.where(
-        "description like ?", "%#{query}%"
+      api_error_instances.where(
+        'api_error_instances.body::text like ?', "%#{query}%"
       )
     )
   end
 
-  def search_responses(_query)
-    # TODO Clément Villain 8/03/18
-    []
+  def search_api_errors(query)
+    @project.api_errors.where(
+      "api_errors.name like ?", "%#{query}%"
+    )
   end
 
-  def search_routes(_query)
-    # TODO Clément Villain 8/03/18
-    []
+  def search_attributes(query)
+    resource_attributes = Attribute.joins(:parent_resource).where(
+      resources: { project: @project }
+    )
+
+    resource_attributes.where(
+      "attributes.name like ?", "%#{query}%"
+    ).or(
+      resource_attributes.where(
+        "attributes.description like ?", "%#{query}%"
+      )
+    )
   end
 
-  def search_description(_query)
-    # TODO Clément Villain 8/03/18
-    []
+  def search_response_headers(query)
+    headers = Header.joins(response: {route: :resource}).where(
+      resources: { project_id: @project.id }
+    )
+
+    search_headers(headers, query)
   end
 
-  def search_schemes(_query)
-    # TODO Clément Villain 8/03/18
-    []
+  def search_request_headers(query)
+    headers = Header.joins(route: :resource).where(
+      resources: { project_id: @project.id }
+    )
+
+    search_headers(headers, query)
   end
 
-  def search_validation_errors(_query)
-    # TODO Clément Villain 8/03/18
-    []
+  def search_headers(headers, query)
+    headers.where(
+      "headers.name like ?", "%#{query}%"
+    ).or(
+      headers.where(
+        "headers.value like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_metadata(query)
+    @project.metadata.where(
+      "metadata.name like ?", "%#{query}%"
+    )
+  end
+
+  def search_mock_pickers(query)
+    mock_pickers = MockPicker.joins(:mock_profile).where(
+      mock_profiles: { project_id: @project.id }
+    )
+
+    mock_pickers.where(
+      "mock_pickers.body_pattern like ?", "%#{query}%"
+    ).or(
+      mock_pickers.where(
+        "mock_pickers.url_pattern like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_mock_profiles(query)
+    @project.mock_profiles.where(
+      "mock_profiles.name like ?", "%#{query}%"
+    )
+  end
+
+  def search_project(query)
+    if @project.description.include? query
+      [@project]
+    else
+      []
+    end
+  end
+
+  def search_query_parameters(query)
+    query_parameters = QueryParameter.joins(route: :resource).where(
+      resources: { project_id: @project.id }
+    )
+
+    query_parameters.where(
+      "query_parameters.name like ?", "%#{query}%"
+    ).or(
+      query_parameters.where(
+        "query_parameters.description like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_reports(query)
+    @project.reports.where(
+      response_status_code: query.to_i
+    ).or(
+      @project.reports.where(
+        "reports.response_body like ?", "%#{query}%"
+      )
+    ).or(
+      @project.reports.where(
+        "reports.request_body like ?", "%#{query}%"
+      )
+    ).or(
+      @project.reports.where(
+        "reports.request_method like ?", "%#{query}%"
+      )
+    ).or(
+      @project.reports.where(
+        "reports.url like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_resource_instances(query)
+    resource_instances = ResourceInstance.joins(:resource).where(
+      resources: { project_id: @project.id }
+    )
+
+    resource_instances.where(
+      "resource_instances.name like ?", "%#{query}%"
+    ).or(
+      resource_instances.where(
+        'resource_instances.body::text like ?', "%#{query}%"
+      )
+    )
+  end
+
+  def search_resource_representations(query)
+    @project.resource_representations.where(
+      "resource_representations.name like ?", "%#{query}%"
+    ).or(
+      @project.resource_representations.where(
+        "resource_representations.description like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_resources(query)
+    @project.resources.where(
+      "resources.name like ?", "%#{query}%"
+    ).or(
+      @project.resources.where(
+        "resources.description like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_responses(query)
+    @project.responses.where(
+      status_code: query.to_i
+    ).or(
+      @project.responses.where(
+        "responses.root_key like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_routes(query)
+    @project.routes.where(
+      "routes.description like ?", "%#{query}%"
+    ).or(
+      @project.routes.where(
+        "routes.url like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_schemes(query)
+    Scheme.where(
+      "schemes.name like ?", "%#{query}%"
+    ).or(
+      Scheme.where(
+        "schemes.regexp like ?", "%#{query}%"
+      )
+    )
+  end
+
+  def search_validation_errors(query)
+    validation_errors = ValidationError.joins(:report).where(
+      reports: { project_id: @project.id }
+    )
+
+    validation_errors.where(
+      "validation_errors.description like ?", "%#{query}%"
+    )
   end
 end
