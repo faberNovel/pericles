06f748e75ded44606b4a28d33eecdb76e08b9335
Clément Villain <clement.villain@fabernovel.com>
2018-07-27 14:54:38 +0200

    Refactor js file in smaller functions
    Change-Id: I541643a2b65843fdf290cb90ba07ff266efae82c

--- app/assets/javascripts/mock_profiles/form.js
+++ app/assets/javascripts/mock_profiles/form.js
@@ -1,30 +1,44 @@
+function createUniqueId() {
+  let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  return randLetter + Date.now();
+}
+
+function setupAdvancedLink(added_element, uniqId ) {
+  let div = added_element[1];
+  div['id'] = uniqId;
+  let a = $(added_element[0]).children('a[href="#-advanced"]')[0];
+  a['href'] = "#" + uniqId;
+}
+
+function setupRegexpField(uniqid) {
+  let $regexp = $('#' + uniqid + ' .regex');
+  $regexp.on('focus', function () {
+    this.innerHTML = this.textContent || this.innerText;
+  });
+
+  $regexp.on('blur', function () {
+    RegexColorizer.colorizeAll();
+
+    target = $(this).data('target');
+    $(this).parent().find("input[id$='" + target + "']").val(this.textContent || this.innerText);
+  });
+}
+
+function afterInsert(event, addedElement) {
+  let uniqid = createUniqueId();
+  setupAdvancedLink(addedElement, uniqid);
+
+  // Regexp field need javascript listeners so if we add some we need to add back the listeners
+  // TODO: Clément Villain 13/12/17 move this code in a function in regex_field gem
+  setupRegexpField(uniqid);
+}
+
 $(document).ready(function() {
-  $('#mock_profile_form').on('cocoon:after-insert', function(event, added_element) {
-    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    var uniqid = randLetter + Date.now();
+  let $mockProfileForm = $('#mock_profile_form');
+  $mockProfileForm.on('cocoon:after-insert', afterInsert);
 
-    var div = added_element[1];
-    div['id'] = uniqid;
-    var a = $(added_element[0]).children('a[href="#-advanced"]')[0];
-    a['href'] = "#" + uniqid;
-
-
-    // Regexp field need javascript listeners so if we add some we need to add back the listeners
-    // TODO: Clément Villain 13/12/17 move this code in a function in regex_field gem
-    $('#' + uniqid + ' .regex').on('focus', function() {
-      this.innerHTML = this.textContent || this.innerText;
-    });
-
-    $('#' + uniqid + ' .regex').on('blur', function() {
-      RegexColorizer.colorizeAll();
-
-      target = $(this).data('target');
-      $(this).parent().find("input[id$='" + target + "']").val(this.textContent || this.innerText);
-    });
-  })
-
-  $('#mock_profile_form').on('cocoon:before-remove', function(event, removed_element) {
-    var collapseId = removed_element.children('a[data-toggle="collapse"]')[0].href.split('#')[1];
+  $mockProfileForm.on('cocoon:before-remove', function(event, removed_element) {
+    let collapseId = removed_element.children('a[data-toggle="collapse"]')[0].href.split('#')[1];
     $('#' + collapseId).remove();
   })
 });
