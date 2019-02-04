35b3d434cc365f27949bd66a9d0cf7c222004c8d
Clément Villain <clement.villain@fabernovel.com>
2019-02-04 09:59:28 +0100

    Destroy instances with api error
    Fix #52329
    
    Change-Id: I1768ac6c97164284aedc4d7fc3bc41322bbad6e1

--- app/models/api_error.rb
+++ app/models/api_error.rb
@@ -1,7 +1,7 @@
 class ApiError < ApplicationRecord
   belongs_to :project
 
-  has_many :api_error_instances
+  has_many :api_error_instances, dependent: :destroy
 
   validates :name, presence: true
   validates :json_schema, json_schema: true
