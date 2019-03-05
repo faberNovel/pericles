dee29fb4a78bd61e52de65513d34ee456071cddd
Clément Villain <clement.villain@fabernovel.com>
2019-03-05 11:57:36 +0100

    Add project_id to existing audits
    Change-Id: Ie4f276066b77c5e0338f6291c51ab12b577434b9

--- db/migrate/20190304112548_add_project_id_to_audits.rb
+++ db/migrate/20190304112548_add_project_id_to_audits.rb
@@ -1,5 +1,14 @@
 class AddProjectIdToAudits < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_reference :audits, :project, foreign_key: true, index: true, null: true
+
+    Audited::Audit.reset_column_information
+    Audited::Audit.find_each do |audit|
+      audit.update(project_id: audit.auditable&.project&.id)
+    end
+  end
+
+  def down
+    remove_reference :audits, :project
   end
 end
