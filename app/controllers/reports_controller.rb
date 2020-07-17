ccf2c3becc53c823fa7597b4df07520109aff82e
Gilles Thia <gilles.thia@fabernovel.com>
2020-07-17 11:43:27 +0200

    Fix #60959 : hide passwords in reports
    Change-Id: I2931784e1d4b75a4c67d1bcb6f4dc0f5d4604421

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,11 +11,20 @@ class ReportsController < ApplicationController
 
   def show
     @report = project.reports.find(params[:id])
+    hide_password_in_bodies(@report.request_body, @report.response_body)
   end
 
   def revalidate
     report = project.reports.find(params[:id])
     ReportValidator.new(report).validate
     redirect_to project_report_path(project, report)
+  end
+
+  private
+
+  def hide_password_in_bodies(*bodies)
+    bodies.each do |body|
+      body&.gsub!(/"password"\s*:\s*"[^"]*"/, '"password":"********"')
+    end
   end
 end
